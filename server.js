ad0db9fb32e6f3c14b03711574d41e0fbd1a8c01
neeraj-tosh <neeraj@toshblocks.com>
2018-10-30 01:46:10 +0530

    heroku changes

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express =require("express");
 const hbs =require("hbs");
 const fs =require('fs');
+const port = process.env.PORT || 3000;
 
 var app = express();
 hbs.registerPartials(__dirname+"/views/partials");
@@ -52,6 +53,6 @@ res.send({
 });
 });
 
-app.listen(3000,()=>{
-	console.log('Server is up and running');
+app.listen(port,()=>{
+	console.log('Server is up and running on '+ port);
 });
